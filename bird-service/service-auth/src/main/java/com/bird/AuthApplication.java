21de33b79b7d57fdbbe20a6211767814eb6c4466
小鸟程序员 <2450107493@qq.com>
2021-09-21 20:01:37 +0800

    搭建日志服务、认证服务

---
+++ bird-service/service-auth/src/main/java/com/bird/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lipu
+ * @Date 2021/9/21 19:57
+ * @Description 认证微服务启动类
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class);
+    }
+}
