8d8b411f9411525bb08b7eb7d92f6d7a8ba5e89a
小鸟程序员 <2450107493@qq.com>
2021-09-21 20:01:31 +0800

    搭建日志服务、认证服务

---
+++ bird-service/service-log/src/main/java/com/bird/LogApplication.java
@@ -0,0 +1,16 @@
+package com.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lipu
+ * @Date 2021/9/14 18:40
+ * @Description 日志微服务启动类
+ */
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class);
+    }
+}
